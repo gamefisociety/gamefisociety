94491d37c6013e8d893fd99e321779c332ff51de
a7freeze <apple7.sve@gmail.com>
2023-02-12 22:31:09 +0800

    test new header

--- src/view/home/GFTHome.jsx
+++ src/view/home/GFTHome.jsx
@@ -5,16 +5,14 @@ import { InjectedConnector } from '@web3-react/injected-connector'
 import { Outlet } from 'react-router-dom'
 
 import './GFTHome.scss';
-import GFTHead from '../head/GFTHead'
+// import GFTHead from '../head/GFTHead'
+import GFTHead01 from '../head/GFTHead01'
 import GFTLeftMenu from '../head/GFTLeftMenu';
-
-
-
 
 function GFTHome() {
     return (
         <div className='home_bg'>
-            <GFTHead></GFTHead>
+            <GFTHead01></GFTHead01>
             <div className='bt_layout'>
                 <GFTLeftMenu></GFTLeftMenu>
                 <Outlet/>
