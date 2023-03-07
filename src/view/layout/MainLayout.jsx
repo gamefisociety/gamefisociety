8efd85be80636a2d6e3fb8b32d7be01603c7a19d
GetDragon1010 <gamefisociety@outlook.com>
2023-03-07 12:39:03 +0800

    [update]profile handle ui

--- src/view/layout/MainLayout.jsx
+++ src/view/layout/MainLayout.jsx
@@ -1,89 +1,97 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Outlet } from "react-router-dom";
 
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Drawer from '@mui/material/Drawer';
-import GCardFriends from 'components/GCardFriends';
-import GCardRelays from 'components/GCardRelays';
-import { setDrawer } from 'module/store/features/dialogSlice';
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Drawer from "@mui/material/Drawer";
+import GCardFriends from "components/GCardFriends";
+import GCardRelays from "components/GCardRelays";
+import { setDrawer } from "module/store/features/dialogSlice";
 
-import GFTHead from 'view/head/GFTHead'
-import GFTLeftMenu from 'view/head/GFTLeftMenu';
+import GFTHead from "view/head/GFTHead";
+import GFTLeftMenu from "view/head/GFTLeftMenu";
 
-import { System } from 'nostr/NostrSystem';
+import { System } from "nostr/NostrSystem";
 import { init } from "module/store/features/loginSlice";
-import { initRelays } from 'module/store/features/profileSlice';
+import { initRelays } from "module/store/features/profileSlice";
 
-import './MainLayout.scss';
+import "./MainLayout.scss";
 
 // const win_h = window.client
 
 const MainLayout = () => {
+  const dispatch = useDispatch();
+  const { isDrawer, placeDrawer, cardDrawer } = useSelector((s) => s.dialog);
 
-    const dispatch = useDispatch();
-    const { isDrawer, placeDrawer, cardDrawer } = useSelector(s => s.dialog);
-
-    return (
-        <Box className='main_bg'>
-            <GFTHead />
-            <Box sx={{
-                width: '280px',
-                position: 'fixed',
-                top: '60px',
-                zIndex: '999',
-            }}>
-                <GFTLeftMenu />
-            </Box>
-            <Grid sx={{ flexGrow: 1 }} container>
-                <Stack
-                    className='main_content'
-                    direction="row"
-                    alignItems={'flex-start'}
-                    justifyContent={'flex-start'}
-                >
-                    <Box sx={{ width: '280px' }} />
-                    <Stack
-                        className={'scroll_content'}
-                        direction="column"
-                        alignItems={'center'}
-                        justifyContent={'flex-start'}
-                    >
-                        <Outlet />
-                    </Stack>
-                </Stack>
-            </Grid>
-            <Drawer
-                PaperProps={{
-                    style:
-                    {
-                        borderRadius: '12px',
-                        backgroundColor: '#0F0F0F'
-                    }
-                }}
-                anchor={placeDrawer}
-                open={isDrawer}
-                onClose={() => {
-                    dispatch(setDrawer({
-                        isDrawer: false,
-                        placeDrawer: 'right',
-                        cardDrawer: 'default'
-                    }))
-                }}
-            >
-                {cardDrawer === 'follow' && <GCardFriends callback={() => {
-                    dispatch(setDrawer({
-                        isDrawer: false,
-                        placeDrawer: 'right',
-                        cardDrawer: 'default'
-                    }))
-                }} />}
-                {cardDrawer === 'relays' && <GCardRelays />}
-            </Drawer>
-        </Box >
-    );
-}
+  return (
+    <Box className="main_bg">
+      <GFTHead />
+      <Box
+        sx={{
+          width: "280px",
+          position: "fixed",
+          top: "60px",
+          zIndex: "999",
+        }}
+      >
+        <GFTLeftMenu />
+      </Box>
+      <Grid sx={{ flexGrow: 1 }} container>
+        <Stack
+          className="main_content"
+          direction="row"
+          alignItems={"flex-start"}
+          justifyContent={"flex-start"}
+        >
+          <Box sx={{ width: "280px" }} />
+          <Stack
+            className={"scroll_content"}
+            direction="column"
+            alignItems={"center"}
+            justifyContent={"flex-start"}
+          >
+            <Outlet />
+          </Stack>
+        </Stack>
+      </Grid>
+      <Drawer
+        PaperProps={{
+          style: {
+            borderRadius: "12px",
+            backgroundColor: "#0F0F0F",
+          },
+        }}
+        anchor={placeDrawer}
+        open={isDrawer}
+        onClose={() => {
+          dispatch(
+            setDrawer({
+              isDrawer: false,
+              placeDrawer: "right",
+              cardDrawer: "default",
+            })
+          );
+        }}
+      >
+        {cardDrawer === "follow" && (
+          <GCardFriends
+            callback={() => {
+              dispatch(
+                setDrawer({
+                  isDrawer: false,
+                  placeDrawer: "right",
+                  cardDrawer: "default",
+                })
+              );
+            }}
+          />
+        )}
+        {cardDrawer === "relays" && <GCardRelays />}
+      </Drawer>
+    </Box>
+  );
+};
 
 export default MainLayout;
