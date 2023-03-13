b7b5935729d58b6dcdbc98c85b09800ae1b0402e
GetDragon1010 <gamefisociety@outlook.com>
2023-03-14 00:04:33 +0800

    fix nostr chat

--- src/view/page/GFTChat.jsx
+++ src/view/page/GFTChat.jsx
@@ -1,12 +1,9 @@
 import { React, useEffect, useState, useRef, useCallback } from "react";
 import { useSelector } from "react-redux";
-import "./GFTChat.scss";
 
-import { bech32 } from "bech32";
-import * as secp from "@noble/secp256k1";
 import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Icon from "@mui/material/Icon";
@@ -17,9 +14,11 @@ import dmLeftImg from "./../../asset/image/social/dm_left.png";
 import { useChatPro } from "nostr/protocal/ChatPro";
 import { System } from "nostr/NostrSystem";
 import { BuildSub } from "nostr/NostrUtils";
-import TimelineCache from 'db/TimelineCache';
-import useNostrEvent from 'nostr/NostrEvent';
-
+import TimelineCache from "db/TimelineCache";
+import useNostrEvent from "nostr/NostrEvent";
+import "./GFTChat.scss";
+const default_avatar =
+  "https://gateway.pinata.cloud/ipfs/Qmd7rgbD9sLRQiMHZRYw1QD4j9WVgBZ3uzdtYehQuXHZq4";
 const ListRow = ({ data, index, setSize, chatPK }) => {
   //
   const rowRef = useRef();
@@ -48,9 +47,10 @@ const ListRow = ({ data, index, setSize, chatPK }) => {
           borderRadius: "6px",
           fontFamily: "Saira",
           fontWeight: "500",
+          fontSize:"12px",
+          color:"#FFFFFF",
+          whiteSpace: 'pre-line'
         }}
-        variant="body2"
-        color="#FFFFFF"
         align={"left"}
       >
         {item.content}
@@ -82,43 +82,51 @@ const GFTChat = (props) => {
   //
   const listenDM = (targetPubkey) => {
     const filterDM = chatPro.get(targetPubkey);
-    let subDM = BuildSub('chat_with', [filterDM]);
+    let subDM = BuildSub("chat_with", [filterDM]);
     TLCache.create(subChat[1]);
     //
     setSubChat(subDM.concat());
     System.BroadcastSub(subDM, (tag, client, msg) => {
-      if (tag === 'EVENT') {
+      if (tag === "EVENT") {
         try {
-          nostrEvent.DecryptData(msg.content, privateKey, targetPubkey).then((dmsg) => {
-            if (dmsg) {
-              let flag = TLCache.pushChat(subChat[1], msg.pubkey, msg.created_at, dmsg);
-              if (flag === false) {
-                return;
+          nostrEvent
+            .DecryptData(msg.content, privateKey, targetPubkey)
+            .then((dmsg) => {
+              if (dmsg) {
+                let flag = TLCache.pushChat(
+                  subChat[1],
+                  msg.pubkey,
+                  msg.created_at,
+                  dmsg
+                );
+                if (flag === false) {
+                  return;
+                }
+                let chat_datas = TLCache.get(subChat[1]);
+                if (chat_datas) {
+                  setChatData(chat_datas.concat());
+                //   scrollToBottom();
+                }
+                // console.log('dm targetPubkey', chat_datas);
               }
-              let chat_datas = TLCache.get(subChat[1]);
-              if (chat_datas) {
-                setChatData(chat_datas.concat());
-              }
-              // console.log('dm targetPubkey', chat_datas);
-            }
-          });
+            });
         } catch (e) {
           //
         }
       }
     });
-  }
+  };
 
   const unlistenDM = (chatPK) => {
-    console.log('unlistenDM', unlistenDM);
+    console.log("unlistenDM", unlistenDM);
     TLCache.clear(subChat[1]);
     System.BroadcastClose(subChat, null, null);
-  }
+  };
   //
   useEffect(() => {
     listenDM(chatPK);
     return () => {
-      unlistenDM()
+      unlistenDM();
     };
   }, [chatPK]);
 
@@ -128,18 +136,28 @@ const GFTChat = (props) => {
     }
     const chatEv = await chatPro.send(chatPK, inValue);
     System.BroadcastEvent(chatEv, (tag, client, msg) => {
-      if (tag === 'OK' && msg.ret && msg.ret === true) {
-        let flag = TLCache.pushChat(subChat[1], publicKey, chatEv.CreatedAt, inValue);
+      if (tag === "OK" && msg.ret && msg.ret === true) {
+        let flag = TLCache.pushChat(
+          subChat[1],
+          publicKey,
+          chatEv.CreatedAt,
+          inValue
+        );
         if (flag === false) {
           return;
         }
         let chat_datas = TLCache.get(subChat[1]);
         if (chat_datas) {
           setChatData(chat_datas.concat());
+          setInValue("");
+          scrollToBottom();
         }
       }
-    }
-    );
+    });
+  };
+
+  const scrollToBottom = () => {
+    listRef.current.scrollToItem(10);
   };
 
   return (
@@ -166,37 +184,72 @@ const GFTChat = (props) => {
         }}
       >
         <Icon
+          className="goback"
           sx={{
             width: "100px",
-            height: "50px",
+            height: "38px",
             display: "flex",
             flexDirection: "row",
             alignItems: "center",
-            justifyContent: "center",
+            justifyContent: "flex-start",
             color: "#FFFFFF",
-            fontSize: "20px",
+            fontSize: "18px",
             fontFamily: "Saira",
             fontWeight: "500",
-          }}
-        >
-          <img src={dmLeftImg} width="50px" alt="dmleft" />
-          DMs
-        </Icon>
-        <Button
-          sx={{
-            width: "60px",
-            height: "60px",
           }}
           onClick={() => {
             props.closeHandle();
           }}
         >
-          <img src={closeImg} width="60px" alt="close" />
+          <img src={dmLeftImg} width="38px" alt="dmleft" />
+          DMs
+        </Icon>
+        <Button
+          sx={{
+            width: "38px",
+            height: "38px",
+          }}
+          onClick={() => {
+            props.closeHandle();
+          }}
+        >
+          <img src={closeImg} width="38px" alt="close" />
         </Button>
+      </Box>
+      <Box
+        sx={{
+          marginTop: "20px",
+          marginBottom: "30px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-start",
+        }}
+      >
+        <Avatar
+          sx={{ width: "40px", height: "40px" }}
+          edge="end"
+          alt="GameFi Society"
+          src={chatProfile.picture ? chatProfile.picture : default_avatar}
+        />
+        <Typography
+          sx={{
+            marginLeft: "8px",
+            fontSize: "14px",
+            fontFamily: "Saira",
+            fontWeight: "500",
+            color: "#FFFFFF",
+          }}
+        >
+          {chatProfile.display_name
+            ? chatProfile.display_name
+            : "Nostr#" + chatPK.substring(chatPK.length - 4, chatPK.length)}
+        </Typography>
       </Box>
       <List
         ref={listRef}
-        height={400}
+        height={500}
         width={"100%"}
         itemSize={getSize}
         itemCount={chatData.length}
@@ -219,7 +272,7 @@ const GFTChat = (props) => {
           width: "100%",
           display: "flex",
           flexDirection: "row",
-          alignItems: "center",
+          alignItems: "flex-start",
           justifyContent: "space-between",
         }}
       >
@@ -232,7 +285,7 @@ const GFTChat = (props) => {
             "& .MuiInputBase-root": {
               color: "white",
               width: "250px",
-              //   height: "40px",
+              // height: "40px",
               fontSize: "18px",
               fontFamily: "Saira",
               fontWeight: "500",
