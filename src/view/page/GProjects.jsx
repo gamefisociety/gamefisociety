d453038f67dc3008d5f4b40a76b552b120bc3f65
GetDragon1010 <gamefisociety@outlook.com>
2023-04-06 18:21:23 +0800

    fix

--- src/view/page/GProjects.jsx
+++ src/view/page/GProjects.jsx
@@ -164,7 +164,18 @@ const GProjects = () => {
 
   const renderWaittingCards = () => {
     return (
-      <Box className={"project_card_contain"}>
+      <Box
+        sx={{
+          paddingTop: "24px",
+          paddingLeft: "24px",
+          paddingRight: "24px",
+          width: "calc( 100% - 48px)",
+          display: "flex",
+          flexWrap: "wrap",
+          gridGap: "32px",
+          gap: "32px",
+        }}
+      >
         {waittingDatas.map((item, index) => {
           return (
             <Stack
@@ -173,13 +184,14 @@ const GProjects = () => {
                 flexDirection: "column",
                 alignItems: "center",
                 justifyContent: "flex-start",
+                width:" calc( 33% - 22.5px ) !important"
               }}
               key={"project_waitting_" + index}
               spacing={1}
             >
               <Skeleton variant="circular" width={60} height={60} />
-              <Skeleton variant="rectangular" width={214} height={60} />
-              <Skeleton variant="rounded" width={214} height={60} />
+              <Skeleton variant="rectangular" width={"100%"} height={60} />
+              <Skeleton variant="rounded" width={"100%"} height={60} />
             </Stack>
           );
         })}
@@ -197,7 +209,7 @@ const GProjects = () => {
         alignItems: "flex-start",
         justifyContent: "flex-start",
         pointerEvents: "all",
-        backgroundColor: "rgba(0, 0, 0, 0.708)",
+        backgroundColor: "rgba(27, 27, 27, 0.95)",
       }}
     >
       {renderTop()}
