b3f56587f03c0b91c10004f6b9bb23ed8bfc43c2
a7freeze <apple7.sve@gmail.com>
2023-04-15 00:29:02 +0800

    FIx

--- src/view/meta/GMetaCamera.jsx
+++ src/view/meta/GMetaCamera.jsx
@@ -7,11 +7,11 @@ import {
 
 export const createCamera = (scene) => {
 
-    var camera = new ArcRotateCamera("camera-main", Math.PI / 3, Math.PI / 3, 500, new Vector3(0, 100, 0), scene);
-    camera.setTarget(new Vector3(0, 100, 0));
+    var camera = new ArcRotateCamera("camera-main", Math.PI / 3, Math.PI / 3, 500, new Vector3(0, 0, 0), scene);
+    camera.setTarget(new Vector3(0, 0, 0));
     // var camera = new ArcFollowCamera("camera-main", Math.PI / 3, Math.PI / 3, 200, new Vector3(0, 100, 0), scene);
-    camera.minZ = 0.1;
-    camera.maxZ = 5000;
+    // camera.minZ = 0.1;
+    // camera.maxZ = 5000;
     //
     const canvas = scene.getEngine().getRenderingCanvas();
     camera.attachControl(canvas, true);
